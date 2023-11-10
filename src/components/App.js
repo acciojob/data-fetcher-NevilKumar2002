e5c68510e957ca023507b1cec8d2d00c93ca0e35
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-11-10 13:36:51 +0000

    First

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,15 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+
+        {/* axios.get("https://dummyjson.com/products")
+        .then((response)=>{
+          console.log(response)
+        } ) */}
+        let ans= fetch("https://dummyjson.com/products")
+        let ans1= ans.json();
+        console.log(ans1)
+        <p>{ans1}</p>
     </div>
   )
 }
